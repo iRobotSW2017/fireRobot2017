ccad948412c302cba9960ee5f53435bf548458f1
unknown <starwarsant05@gmail.com>
2018-02-10 09:47:59 -0500

    TTYL

---
+++ ac/TerryTTac.c
@@ -0,0 +1,18 @@
+
+task main()
+{
+while(1==1)
+{
+	turnLEDOn(green);
+	Wait(5);
+	turnLEDOff(green);
+
+	turnLEDOn(yellow);
+	Wait(1);
+	turnLEDOff(yellow);
+
+	turnLEDOn(red);
+	Wait(5);
+	turnLEDOff(red);
+
+}
